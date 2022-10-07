9e407ab7796dcaa4fb4177b0765fdcfb8866d30b
20Nikita <gordeev.2017@bk.ru>
2022-10-07 14:03:51 +0300

    Сделал основу уравня как перемещающуюся трубку

--- Assets/My_worck/scripts/spawn.cs
+++ Assets/My_worck/scripts/spawn.cs
@@ -3,24 +3,33 @@ using UnityEngine;
 public class spawn : MonoBehaviour
 {
     public GameObject spawnPoint;
-    public GameObject objToSpawn;
-    public bool Spawn = false;
-    public float timeSpawn = 1f;
-    float time = 0;
+    private int N = 15;
+    public float let_item = 100;
+    private GameObject[] lewel = new GameObject[100];
+    private float speed = 20f;
+
     // Update is called once per frame
     void Update()
     {
-        time += Time.deltaTime;
-        if (Spawn & time > timeSpawn) {
-            time = 0;
-            Instantiate(objToSpawn, spawnPoint.transform.position, Quaternion.identity);
+
+        for (int i = 0; i < N; i++) {
+            lewel[i].transform.position = lewel[i].transform.position + new Vector3(0, 0, -speed * Time.deltaTime);
+            if (lewel[i].transform.position.z <= -100) {
+                int k = i - 1;
+                if (k < 0) k = N - 1;
+                Vector3 p = lewel[k].transform.position;
+                p.z = p.z + let_item;
+                lewel[i].transform.position = p;
+
+            }
+
         }
-        
+
     }
-    public void startSpawn() {
-        Spawn = true;
+    void Start()
+    {
+        for(int i = 0; i < N; i++)
+            lewel[i] = Instantiate(spawnPoint, new Vector3(0, 0, i * let_item), Quaternion.identity);
     }
-    public void stoptSpawn() {
-        Spawn = false;
-    }
+   
 }
